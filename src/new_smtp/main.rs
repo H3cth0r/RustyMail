94bd90ae569e8f18581db1651be28202a1249f5f
h3cth0r <hectorsucre13@gmail.com>
2024-07-20 21:41:49 -0600

    Implemented DNS loopkup

--- src/new_smtp/main.rs
+++ src/new_smtp/main.rs
@@ -1,7 +1,15 @@
 mod commands;
 mod smtp;
 mod server;
+mod dns_lookup;
+
+// fn main() {
+//     let result = dns_lookup::lookup_mx_record("hector.miranda@zentinel.mx");
+//     println!("{:?}", result);
+// }
 
 fn main() -> std::io::Result<()> {
-    server::start_server("127.0.0.1:2525")
+    // server::start_server("127.0.0.1:2525")
+    let result = dns_lookup::lookup_mx_record("hector.miranda@zentinel.mx");
+    println!("{:?}", result);
 }
